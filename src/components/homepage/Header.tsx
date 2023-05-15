679ef93a8945adb2fdb31cd92bada910e1b2142a
tomashrib <tomas.hrib@proton.me>
2023-05-15 21:33:25 +0200

    feat: add 'Blog' button to header

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 const HeaderWrapper = styled.header`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   padding: 2rem 0;
   background-color: #131314;
   width: 100%;
@@ -16,12 +16,28 @@ const HeaderWrapper = styled.header`
     align-self: flex-start;
     padding-left: 1rem;
   }
+  .blog {
+    font-size: 1.5rem;
+    padding-right: 4rem;
+
+    a {
+      text-decoration: none;
+      color: white;
+
+      &:hover {
+        border-bottom: 2px solid white;
+      }
+    }
+  }
 `;
 
 const Header = () => {
   return (
     <HeaderWrapper>
       <div className="title">SIML.AI</div>
+      <div className="blog">
+        <a href="/blog">Blog</a>
+      </div>
     </HeaderWrapper>
   );
 };

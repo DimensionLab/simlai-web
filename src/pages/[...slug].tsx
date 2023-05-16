78577c59226f417a53cf8968b9b8e206e7cd2765
tomashrib <tomas.hrib@proton.me>
2023-05-16 12:54:04 +0200

    feat: add [...slug].tsx for dynamic rendering of pages on blog

---
+++ src/pages/[...slug].tsx
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from '@storyblok/react';
+
+export default function Page({ story  }) {
+  story = useStoryblokState(story);
+
+  return (
+    <div>
+      <Head>
+        <title>{story ? story.name : 'My Site'}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <StoryblokComponent blok={story.content} />
+      </Layout>
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  let slug = params.slug ? params.slug.join('/') : 'blog';
+
+  let sbParams = {
+    version: 'draft', // or 'published'
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+    },
+    revalidate: 3600,
+  };
+}
+
+export async function getStaticPaths() {
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get('cdn/links/', {
+    version: 'draft',
+  });
+
+  let paths: any = [];
+  Object.keys(data.links).forEach((linkKey) => {
+    if (data.links[linkKey].is_folder || data.links[linkKey].slug === 'home') {
+      return;
+    }
+
+    const slug = data.links[linkKey].slug;
+    let splittedSlug = slug.split('/');
+
+    paths.push({ params: { slug: splittedSlug } });
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}

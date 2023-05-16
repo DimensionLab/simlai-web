af791f80e23ad1dacae987bffec7ef353fed011e
tomashrib <tomas.hrib@proton.me>
2023-05-16 19:51:41 +0200

    feat: center components in article storyblok component

--- src/components/storyblok-components/Article.js
+++ src/components/storyblok-components/Article.js
@@ -2,6 +2,37 @@ import { render } from 'storyblok-rich-text-react-renderer';
 import styled from 'styled-components';
 
 const ArticleWrapper = styled.article`
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem 1rem 2rem 1rem;
+
+    .img-container {
+      img{
+        width: 100%;
+      }
+    }
+    .article-content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      h1{
+        font-size: 1.5rem;
+      }
+      h2{
+        font-size: 1rem;
+        color: gray;
+      }
+      .article-text {
+        width: 90%;
+        text-align: left;
+        max-width: 800px;
+      }
+    }
+  }
 `;
 
 const Article = ( { blok } ) => {

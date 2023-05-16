1923ac05ae9a7e6758bf676b3c83ec4e167dbb30
tomashrib <tomas.hrib@proton.me>
2023-05-16 20:17:55 +0200

    feat: add styling to images in article

--- src/components/storyblok-components/Article.js
+++ src/components/storyblok-components/Article.js
@@ -2,7 +2,7 @@ import { render } from 'storyblok-rich-text-react-renderer';
 import styled from 'styled-components';
 
 const ArticleWrapper = styled.article`
-  padding-top: 5rem;
+  padding-top: 2rem;
   padding-bottom: 5rem;
   .container {
     display: flex;
@@ -11,8 +11,12 @@ const ArticleWrapper = styled.article`
     padding: 2rem 1rem 2rem 1rem;
 
     .img-container {
+      display: flex;
+      justify-content: center;
       img{
         width: 100%;
+        border-radius: 10px;
+        max-width: 800px;
       }
     }
     .article-content {
@@ -20,16 +24,22 @@ const ArticleWrapper = styled.article`
       flex-direction: column;
       align-items: center;
       h1{
-        font-size: 1.5rem;
+        font-size: 2.5rem;
       }
       h2{
         font-size: 1rem;
         color: gray;
       }
       .article-text {
-        width: 90%;
+        width: 100%;
         text-align: left;
         max-width: 800px;
+
+        img {
+          width: 100%;
+          max-width: 800px;
+          max-height: 480px;
+        }
       }
     }
   }
@@ -39,14 +49,14 @@ const Article = ( { blok } ) => {
   return (
     <ArticleWrapper>
       <div className='container'>
-        <div className='img-container'>
-          <img src={blok.image.filename} alt={blok.image.alt}/>
-        </div>
         <div className='article-content'>
           <h1>{blok.title}</h1>
           <h2>{blok.subtitle}</h2>
           <div className='article-text'>
             {render(blok.content)}
+          </div>
+          <div className='img-container'>
+            <img src={blok.image.filename} alt={blok.image.alt}/>
           </div>
         </div>
       </div>

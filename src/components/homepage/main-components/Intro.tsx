a50d3a5727de9aa1b21a9b039ebd4ec2359d8c0a
tomashrib <tomas.hrib@proton.me>
2023-06-03 15:58:14 +0200

    feat: add image and points instead of slider (temporarily) [mobile]

--- src/components/homepage/main-components/Intro.tsx
+++ src/components/homepage/main-components/Intro.tsx
@@ -11,7 +11,8 @@ const IntroWrapper = styled.div`
     px-3
     py-5
     flex
-    flex-row
+    flex-col
+    gap-y-10
   `}
 
   img {
@@ -19,13 +20,50 @@ const IntroWrapper = styled.div`
       w-[60%]
     `}
   }
+
+  .slider {
+    ${tw`
+      flex
+      flex-col
+      w-full
+    `}
+
+    img {
+      ${tw`
+        w-full
+      `}
+    }
+    .buttons {
+      ${tw`
+        flex
+        flex-row
+        gap-x-2
+        justify-center
+      `}
+      img{
+        ${tw`
+          w-3
+        `}
+      }
+    }
+  }
 `;
 
 const Intro = () => {
+  const pics = 8;
+
   return (
     <IntroWrapper>
       <img src="assets/simlai/simlai-title.svg" alt="" />
       {/* <ImageSlider/>  */}
+      <div className="slider">
+        <img src="assets/simlai/intro-slider-images/image1.svg" alt="" />
+        <div className="buttons">
+          {Array.from({ length: pics }).map((_, index) => (
+          <img key={index} src="assets/simlai/intro-slider-images/unfocused-point.svg"/>
+          ))}
+        </div>
+      </div>
     </IntroWrapper>
   );
 }

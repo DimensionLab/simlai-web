705cd4cbfeedfa35197e285137fb2795f08b296e
tomashrib <tomas.hrib@proton.me>
2023-06-08 22:12:13 +0200

    feat: use dynamic imports for mobile/desktop versions of Intro

--- src/components/homepage/main-components/IntroParent.tsx
+++ src/components/homepage/main-components/IntroParent.tsx
@@ -1,6 +1,11 @@
-import { useEffect, useState, FunctionComponent } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import dynamic from 'next/dynamic';
+
+// Dynamic imports
+const Intro = dynamic(() => import('./mobile-components/Intro'), { ssr: false });
+const IntroDesktop = dynamic(() => import('./desktop-components/IntroDesktop'), { ssr: false });
 
 const IntroParentWrapper = styled.div`
   .loading {
@@ -17,49 +22,20 @@ const IntroParentWrapper = styled.div`
 `;
 
 const IntroParent = () => {
-  const [width, setWidth] = useState(0);
-  const [Component, setComponent] = useState<FunctionComponent | null>(null);
-  const tailwindDesktopBreakpoint = 1280;
+  const [isDesktop, setIsDesktop] = useState(false);
 
   useEffect(() => {
-    setWidth(window.innerWidth);
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+    // Update the state depending on window width
+    setIsDesktop(window.innerWidth >= 1280);
+    // Listen for window resize
+    window.addEventListener('resize', () => {
+      setIsDesktop(window.innerWidth >= 1280);
+    });
   }, []);
-
-  useEffect(() => {
-    if (width > tailwindDesktopBreakpoint) {
-      import("./desktop-components/IntroDesktop").then((module) => {
-        setComponent(() => module.default);
-      });
-    } else {
-      import("./mobile-components/Intro").then((module) => {
-        setComponent(() => module.default);
-      });
-    }
-  }, [width]);
-
-  if (!Component) {
-    // return (
-    //   <IntroParentWrapper>
-    //     <div className="loading">
-    //       <img src="assets/simlai/simlai-logo.svg" alt=""/>
-    //     </div>
-    // </IntroParentWrapper>
-    // )
-    return null;
-  }
 
   return (
     <IntroParentWrapper>
-      <Component />
+      {isDesktop ? (<IntroDesktop/>) : (<Intro/>)}
     </IntroParentWrapper>
   );
 };

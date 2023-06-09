015ce13ee68966d4a199ddb56ab7ee07f17ddcc4
tomashrib <tomas.hrib@proton.me>
2023-06-09 20:55:01 +0200

    feat: limit width of some components in Main and on desktop layout make TryPlatform and Funding into grid of 2 columns

--- src/components/homepage/Main.tsx
+++ src/components/homepage/Main.tsx
@@ -11,31 +11,67 @@ import Funding from "./main-components/Funding";
 import IntroParent from "./main-components/IntroParent";
 
 const MainWrapper = styled.main`
+  // .width-limitedd {
+  //   ${tw`
+  //     text-3xl
+  //     container
+  //     bg-[#0D101B]
+  //     w-full
+  // `}
+  // }
+  // display: grid;
+  // grid-auto-flow: row;
+  // align-items: center;
+  // justify-items: center;
+  // min-height: 90vh;
+
   ${tw`
-    text-3xl
-    container
-    bg-[#0D101B]
     w-full
+    flex
+    flex-col
+    items-center
   `}
-  display: grid;
-  grid-auto-flow: row;
-  align-items: center;
-  justify-items: center;
-  min-height: 90vh;
+
+  .width-limited {
+    ${tw`
+      container
+    `}
+  }
+
+  .not-width-limited {
+    ${tw`
+      w-full
+    `}
+  }
+
+  #try-funding {
+    ${tw`
+      xl:grid
+      xl:grid-cols-2
+      xl:my-52
+    `}
+  }
+
 `;
 
 const Main = () => {
   return (
     <MainWrapper>
-      <IntroParent/>
-      <SignUp/>
-      <Features/>
-      <Product/>
-      <ModelEngineer/>
-      <SimulationStudio/>
-      <Message/>
-      <TryPlatform/>
-      <Funding/>
+      <div className="width-limited">
+        <IntroParent/>
+        <SignUp/>
+        <Features/>
+        <Product/>
+        <ModelEngineer/>
+        <SimulationStudio/>
+      </div>
+      <div className="not-width-limited">
+        <Message/>
+      </div>
+      <div className="width-limited" id="try-funding">
+        <TryPlatform/>
+        <Funding/>
+      </div>
     </MainWrapper>
   );
 };

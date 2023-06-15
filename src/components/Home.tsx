cf6a96ad6371b78d55d1b7646f51416f2add940d
tomashrib <tomas.hrib@proton.me>
2023-06-16 00:11:45 +0200

    feat: import Inter font to website and apply it [#20]

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,11 +16,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Siml.ai</title>
-        <meta name="description" content="Landing page for Siml.ai" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="assets/simlai/simlai-logo.svg" />
+          <title>Siml.ai</title>
+          <meta name="description" content="Landing page for Siml.ai" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="assets/simlai/simlai-logo.svg" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
+
 
       <Layout>
         {!isOpen ? (

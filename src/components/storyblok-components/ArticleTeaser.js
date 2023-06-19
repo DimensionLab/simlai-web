8dc4a8c0d89880579df83565510c7ec3518404a0
tomashrib <tomas.hrib@proton.me>
2023-06-19 15:14:49 +0200

    feat: limit height of image in article teaser

--- src/components/storyblok-components/ArticleTeaser.js
+++ src/components/storyblok-components/ArticleTeaser.js
@@ -7,8 +7,8 @@ const ArticleTeaser = ({ Article }) => {
       <div className="w-full px-4">
         <Link href={`/blog/${Article.slug}`}>
           <a className="w-full flex flex-col items-center h-full">
-            <div className="flex w-full h-1/2">
-              <img src={Article.image.filename} alt="blog" className="h-1/2 rounded-t-xl"/>
+            <div className="flex w-full">
+              <img src={Article.image.filename} alt="blog" className="w-full aspect-video object-cover rounded-t-xl"/>
             </div>
             <div className="h-1/2 flex flex-col w-full px-6 py-6 gap-y-6 bg-[#222530] rounded-b-xl">
               <h1 className="flex self-start w-full text-xl">{Article.title}</h1>

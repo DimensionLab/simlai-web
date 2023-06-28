956ca0d5e0ba0f501d8a7ef79ef0a3841616d7a2
tomashrib <tomas.hrib@proton.me>
2023-06-28 16:39:12 +0200

    refactor: center newest article, adjust width ratios and fix overflows

--- src/components/storyblok-components/NewestArticleTeaser.tsx
+++ src/components/storyblok-components/NewestArticleTeaser.tsx
@@ -1,12 +1,11 @@
 const NewestArticleTeaser = ( { Article }: any) => {
-    console.log(Article);
     return (
-        <section>
-            <article>
-                <div>
-                    <img src={Article.content.image.filename} alt={Article.content.image.alt} />
+        <section className="">
+            <article className="flex flex-row">
+                <div className="w-3/4 overflow-hidden flex">
+                    <img src={Article.content.image.filename} alt={Article.content.image.alt} className="rounded-l-xl object-cover aspect-video w-full"/>
                 </div>
-                <div className="h-1/2 flex flex-col w-full px-6 py-12 gap-y-6 bg-[#222530] rounded-b-xl">
+                <div className="flex flex-col w-1/4 px-6 py-12 gap-y-6 bg-[#222530] rounded-r-xl justify-between">
                     <h1 className="flex self-start w-full text-xl">{Article.content.title}</h1>
                     <div className="flex w-full justify-between items-center">
                         <span className="text-sm text-[#7C7F8B]">Model Engineer</span>

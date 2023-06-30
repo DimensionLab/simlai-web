80d74b9f6d73a0d4f6c34bfce8436e68488e0980
tomashrib <tomas.hrib@proton.me>
2023-06-30 13:23:05 +0200

    refactor: add key props and rename to lowercase

--- src/components/storyblok-components/ArticleTeaser.tsx
+++ src/components/storyblok-components/ArticleTeaser.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-const ArticleTeaser = ({ Article }: any) => {
+const ArticleTeaser = ({ article }: any) => {
   return (
-    <section className="w-full py-2 h-full">
+    <section className="w-full py-2 h-full" key={article.uuid}>
       <div className="w-full px-4 h-full">
-        <Link href={`/blog/${Article.slug}`}>
+        <Link href={`/blog/${article.slug}`}>
           <a className="w-full flex flex-col items-center h-full">
             <div className="flex w-full">
-              <img src={Article.image.filename} alt="blog" className="w-full aspect-video object-cover rounded-t-xl"/>
+              <img src={article.image.filename} alt="blog" className="w-full aspect-video object-cover rounded-t-xl"/>
             </div>
             <div className="h-1/2 flex flex-col w-full px-6 py-12 gap-y-6 bg-[#222530] rounded-b-xl flex-grow justify-between">
-              <h1 className="flex self-start w-full text-xl">{Article.title}</h1>
+              <h1 className="flex self-start w-full text-xl">{article.title}</h1>
               <div className="flex w-full justify-between items-center">
                 <span className="text-sm text-[#7C7F8B]">Model Engineer</span>
                 <img src="/assets/simlai/arrow-blog-teaser.svg"></img>
--- src/components/storyblok-components/NewestArticleTeaser.tsx
+++ src/components/storyblok-components/NewestArticleTeaser.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-const NewestArticleTeaser = ( { Article }: any) => {
+const NewestArticleTeaser = ( { article }: any) => {
     return (
-        <section className="w-full">
-            <Link href={`/blog/${Article.content.slug}`}>
+        <section className="w-full" key={article.uuid}>
+            <Link href={`/blog/${article.content.slug}`}>
                 <a>
                     <article className="flex flex-row">
                         <div className="w-3/4 overflow-hidden flex">
-                            <img src={Article.content.image.filename} alt={Article.content.image.alt} className="rounded-l-xl object-cover aspect-video w-full"/>
+                            <img src={article.content.image.filename} alt={article.content.image.alt} className="rounded-l-xl object-cover aspect-video w-full"/>
                         </div>
                         <div className="flex flex-col w-1/4 px-6 py-12 gap-y-6 bg-[#222530] rounded-r-xl justify-between">
-                            <h1 className="flex self-start w-full text-xl">{Article.content.title}</h1>
+                            <h1 className="flex self-start w-full text-xl">{article.content.title}</h1>
                             <div className="flex w-full justify-between items-center">
                                 <span className="text-sm text-[#7C7F8B]">Model Engineer</span>
                                 <img src="/assets/simlai/arrow-blog-teaser.svg"></img>

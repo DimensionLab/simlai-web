8a830c1e34142d603c927cd1d5369a32494e7325
tomashrib <tomas.hrib@proton.me>
2023-07-03 11:48:03 +0200

    refactor: move font import to _document and customize page titles and meta tag descriptions

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -105,15 +105,9 @@ export default function Blog( props: any ) {
   return (
     <>
       <Head>
-            <title>Siml.ai</title>
-            <meta name="description" content="Landing page for Siml.ai" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="assets/simlai/simlai-logo.svg" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <title>Blog | Siml.ai</title>
+        <meta name="description" content="Siml.ai Blog" />
       </Head>
-        
       <Layout>
           <section className="flex flex-col w-full h-full">
             <div className={`w-full h-full ${!isOpen ? `hidden` : `flex flex-col justify-between`}`}>

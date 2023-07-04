ce008743d413983f7e91afa6de1839305867d47b
Michal Takáč <takacmichal92@gmail.com>
2023-07-04 14:53:22 +0200

    fix: change the GA implementation a bit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,20 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2TRPR38KHZ" strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+    <><Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-2TRPR38KHZ');
-        `}
-      </Script>
+          gtag('config', 'G-2TRPR38KHZ', {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
       <Component {...pageProps} />
     </>
   )

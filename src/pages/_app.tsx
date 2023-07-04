18cb899df3048fa824539a06e642349e9e85e26d
Michal Takáč <takacmichal92@gmail.com>
2023-07-04 14:56:38 +0200

    chore: just a test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-2TRPR38KHZ', {
+          gtag('config', 'G-2E4LDXYBNV', {
             page_path: window.location.pathname,
           });
         `,

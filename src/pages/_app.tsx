48f172c380d8996d32ba22698c9b40fb47be2f84
Michal Takáč <takacmichal92@gmail.com>
2023-07-04 16:36:38 +0200

    feat: use Vercel analytics instead of google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,13 @@
 import "../global.css";
 import "../loading.css";
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <><Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-2E4LDXYBNV', {
-            page_path: window.location.pathname,
-          });
-        `,
-      }}
-    />
+    <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }

57ad3190b0aa76bc7426fa6a74f2eb4a4f0e1561
tomashrib <tomas.hrib@proton.me>
2023-07-05 14:17:25 +0200

    feat: generate a sitemap + scripts to run it

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://siml.ai',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }

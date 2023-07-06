4911c6545709bf302e384b0e3ef4531e1d150f8e
tomashrib <tomas.hrib@proton.me>
2023-07-06 12:29:24 +0200

    refactor: make cards responsive

--- src/components/homepage/main-components/cards/EngagementCard.tsx
+++ src/components/homepage/main-components/cards/EngagementCard.tsx
@@ -11,6 +11,8 @@ interface EngagementCardProps {
 const BgWrapper = styled.div<{ bgColor: string}>`
     background: ${props => props.bgColor};
     border-radius: 0.8rem;
+    width: 100%;
+    height: 100%;
 `;
 
 const EngagementCard = (props: EngagementCardProps) => {
@@ -19,8 +21,8 @@ const EngagementCard = (props: EngagementCardProps) => {
     return (
         <section className="flex w-full h-full">
             <BgWrapper bgColor={props.bgColor}>
-                <main className="flex flex-col gap-y-8 py-16 px-4">
-                    <h1 className="text-3xl font-bold">{props.title}</h1>
+                <main className="flex flex-col gap-y-8 py-16 px-4 w-full justify-between h-full">
+                    <h1 className="text-3xl font-bold lg:text-5xl">{props.title}</h1>
                     <h2 className="text-xl">{props.subtitle}</h2>
                     <a href={props.buttonHref} className="text-sm px-6 py-4 border rounded w-fit font-bold hover:bg-white hover:text-black duration-[0.6s]">{props.buttonText}</a>
                 </main>

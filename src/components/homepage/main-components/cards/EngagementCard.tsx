bc6a2688ffaa0ad342446d8ce669de24d567fca0
tomashrib <tomas.hrib@proton.me>
2023-07-06 12:46:53 +0200

    refactor: change paddings

--- src/components/homepage/main-components/cards/EngagementCard.tsx
+++ src/components/homepage/main-components/cards/EngagementCard.tsx
@@ -24,7 +24,7 @@ const EngagementCard = (props: EngagementCardProps) => {
                 <main className="flex flex-col gap-y-8 py-16 px-4 w-full justify-between h-full">
                     <h1 className="text-3xl font-bold lg:text-5xl">{props.title}</h1>
                     <h2 className="text-xl">{props.subtitle}</h2>
-                    <a href={props.buttonHref} className="text-sm px-6 py-4 border rounded w-fit font-bold hover:bg-white hover:text-black duration-[0.6s]">{props.buttonText}</a>
+                    <a href={props.buttonHref} className="text-sm px-4 py-[11px] border rounded w-fit font-bold hover:bg-white hover:text-black duration-[0.6s]">{props.buttonText}</a>
                 </main>
             </BgWrapper>
         </section>

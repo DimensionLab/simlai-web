eb617b814aa1fc45a5dffcaa4a2c90c6628eb3a1
tohrxyz <tomas.hrib@proton.me>
2023-07-25 00:08:34 +0200

    refactor: disable button and input field + make whole category field clickable

--- src/components/blog-components/Search.tsx
+++ src/components/blog-components/Search.tsx
@@ -10,12 +10,12 @@ const Search = ( { categoryArr, setSelected }: SearchProps ) => {
   const [categoryOpened, setCategoryOpened] = useState(false);
   const [defaultCategoryText, setDefaultCategoryText] = useState("Categories");
 
-  const handleSubmit = () => {
-    setButtonText("Not yet");
-    setTimeout(
-      () => setButtonText("Submit"), 2000
-    );
-  }
+  // const handleSubmit = () => {
+  //   setButtonText("Not yet");
+  //   setTimeout(
+  //     () => setButtonText("Submit"), 2000
+  //   );
+  // }
 
   const handleCategoryOpened = () => {
     setCategoryOpened(!categoryOpened);
@@ -34,13 +34,13 @@ const Search = ( { categoryArr, setSelected }: SearchProps ) => {
         <div className="flex flex-col gap-y-4 xl:flex-row max-w-3xl xl:gap-x-4">
           <div className="flex flex-col gap-y-2 xl:w-[40%]">
             <label className="text-[#7C7F8B] text-xs">SEARCH BY KEYWORD</label>
-            <input type="text" placeholder="Input text" className="bg-[#373A45] py-4 px-4 rounded text-sm text-[#7C7F8B]" disabled/>
+            <input type="text" placeholder="Input text" className="bg-[#373A45] py-4 px-4 rounded text-sm text-[#7C7F8B] hover:cursor-not-allowed" disabled/>
           </div>
           <div className="flex flex-col xl:w-[40%] gap-y-0 relative">
             <label className="text-[#7C7F8B] text-xs pb-2">FILTER BY CATEGORY</label>
-            <div className="bg-[#373A45] py-4 px-4 rounded-t text-sm text-white flex justify-between items-center">
+            <div className="bg-[#373A45] py-4 px-4 rounded-t text-sm text-white flex justify-between items-center hover:cursor-pointer" onClick={handleCategoryOpened}>
               <span className={`text-[#7C7F8B]`}>{defaultCategoryText}</span>
-              <img src="assets/simlai/dropdown-icon.svg" alt="dropdown icon" onClick={handleCategoryOpened}/>
+              <img src="assets/simlai/dropdown-icon.svg" alt="dropdown icon"/>
             </div>
             <div className={`${categoryOpened ? `absolute w-full mt-[4.7rem] max-sm:bg-black/80 pb-4` : ``}`}>
               {categoryArr?.map((category, index) => (
@@ -48,7 +48,7 @@ const Search = ( { categoryArr, setSelected }: SearchProps ) => {
               ))}
             </div>
           </div>
-          <button className={`w-full bg-[#61646F] py-3.5 px-4 rounded  xl:items-center xl:justify-center xl:w-24 ${categoryOpened ? `hidden` : `xl:flex xl:self-end`}`} onClick={handleSubmit}>{buttonText}</button>
+          <button className={`w-full hover:cursor-not-allowed bg-[#61646F] py-3.5 px-4 rounded  xl:items-center xl:justify-center xl:w-24 ${categoryOpened ? `hidden` : `xl:flex xl:self-end`}`} disabled>{buttonText}</button>
         </div>
       </main>
     </section>

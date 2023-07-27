29f4e7e24917e0a870a0896885921c28cfb44b1f
tohrxyz <tomas.hrib@proton.me>
2023-07-27 17:13:45 +0200

    feat: add embedded video with logic for parsing url

--- src/components/storyblok-components/UniPost.tsx
+++ src/components/storyblok-components/UniPost.tsx
@@ -2,58 +2,70 @@ import { render } from 'storyblok-rich-text-react-renderer';
 import styled from 'styled-components';
 import 'prismjs/themes/prism-twilight.css';
 import Prism from 'prismjs';
-import { useEffect } from "react";
-import { ArticleStoryblok } from '../../../component-types-sb';
+import { useEffect, useState } from "react";
 
 const ArticleWrapper = styled.article``;
 
+interface VideoType {
+    url: string,
+    title: string,
+}
+
 const UniPost = ( { blok }: any) => {
+    const [videoData, setVideoData] = useState<VideoType>()
+
     useEffect(() => {
       Prism.highlightAll();
-  }, [blok.content]);
+    }, [blok.content]);
 
-  // personal creative decision, if there's ":" in title, add new line
-  // i think it looks better, will see when it breaks
-  const handleTitle = () => {
-    let title = String(blok.title);
-    title = title.replace(/:/g, ':</br>');
-    return title;
-  }
+    useEffect(() => {
+        const getVideoData = async () => {
+            const response = await fetch(`https://www.youtube.com/oembed?url=${blok.video_link.url}`)
+            const data = await response.json();
+    
+            const srcRegex = /src="([^"]+)"/;
+            const matches = data.html.match(srcRegex);
+            
+            let srcValue = "";
+            if (matches && matches.length >= 2) {
+                srcValue = matches[1];
+            } else {
+                console.log("No 'src' attribute found or the URL is invalid.");
+            }
+    
+            const info = {
+                url: srcValue,
+                title: data.title
+            }
+    
+            setVideoData(info);
+        }
+
+        getVideoData();
+    }, []);
 
   return (
     <ArticleWrapper>
-      <div className='w-full flex items-center justify-center pb-24 pt-12 lg:pt-24'>
-        <div className='article-content flex flex-col gap-y-8 items-center w-[80%]'>
-          <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl max-w-5xl flex self-start">
-            {blok.title}
-          </h1>
-          {/* <div className='grid w-full pt-8 self-start -mt-10 gap-x-2 grid-cols-3 max-w-md text-sm md:text-lg'>
-            <div className='w-full flex flex-col'>
-              <span className='text-[#454853]'>Date:</span>
-              <span className='text-[#7C7F8B]'>{blok.date}</span>
+        <div className='w-full flex items-center justify-center pb-24 pt-12 lg:pt-24'>
+            <div className='article-content flex flex-col gap-y-8 items-center w-[80%]'>
+                <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl max-w-5xl flex self-center">
+                    {blok.title}
+                </h1>
+                {/* <div className='img-container w-full max-w-4xl lg:py-10'>
+                    <BorderAroundMainImage>
+                        {blok.thumbnail && blok.thumbnail.filename && <img src={blok.thumbnail.filename} alt={blok.thumbnail.alt || ''} className='rounded-xl' />}
+                    </BorderAroundMainImage>
+                </div> */}
+                <ImageWrapper className='max-w-4xl'>
+                    <iframe className="aspect-video w-full" src={videoData?.url} title={videoData?.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </ImageWrapper>
+                <div className='article-text text-md w-full max-w-4xl flex flex-col gap-y-10 items-center'>
+                    <RichTextArticleContent>
+                        {render(blok.description_content)}
+                    </RichTextArticleContent>
+                </div>
             </div>
-            <div className='w-full flex flex-col'>
-              <span className='text-[#454853]'>Category:</span>
-              <span className='text-[#7C7F8B]'>{blok.category}</span>
-            </div>
-            <div className='w-full flex flex-col'>
-              <span className='text-[#454853]'>Author:</span>
-              <span className='text-[#7C7F8B]'>{blok.author ? `${blok.author}` : "DimensionLab"}</span>
-            </div>
-          </div> */}
-          {/* <h2>{blok.subtitle}</h2> */}
-          <div className='img-container w-full max-w-4xl lg:py-10'>
-            <BorderAroundMainImage>
-              {blok.thumbnail && blok.thumbnail.filename && <img src={blok.thumbnail.filename} alt={blok.thumbnail.alt || ''} className='rounded-xl' />}
-            </BorderAroundMainImage>
-          </div>
-          <div className='article-text text-md w-full max-w-4xl flex flex-col gap-y-10 items-center'>
-            <RichTextArticleContent>
-              {render(blok.description_content)}
-            </RichTextArticleContent>
-          </div>
         </div>
-      </div>
     </ArticleWrapper>
   );
 };
@@ -124,3 +136,23 @@ const RichTextArticleContent = styled.article`
     }
   }
 `;
+
+const ImageWrapper = styled.div`
+  background: linear-gradient(180deg, #C063F9 0%, #8B7CFF 100%);
+  border-radius: 0.8rem;
+  padding: 0.4rem;
+  width: 100%;
+  
+  @media (min-width: 1024px) {
+    padding: 1.3rem;
+  }
+
+  iframe {
+    border: 5px solid rgba(59, 17, 81, 0.40);
+    border-radius: 0.8rem;
+
+    @media(min-width: 1024px) {
+      border: 10px solid rgba(59, 17, 81, 0.40);
+    }
+  }
+`

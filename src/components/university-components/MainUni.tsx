042342befc8dd69c7f9944da1fb3ce6e4214a575
tohrxyz <tomas.hrib@proton.me>
2023-07-27 16:27:02 +0200

    refactor: customize teaser for uni post and enlarge width

--- src/components/university-components/MainUni.tsx
+++ src/components/university-components/MainUni.tsx
@@ -9,7 +9,7 @@ const MainUni = (props: MainUniProps) => {
   const sbStory = props.story;
 
   return (
-      <main className="flex flex-col justify-center items-left h-full w-full px-4 py-8 md:py-24 gap-y-4 max-w-4xl">
+      <main className="flex flex-col justify-center items-left h-full w-full px-4 py-8 md:py-24 gap-y-4 max-w-6xl">
           <h1 className="font-bold text-3xl md:text-5xl">Welcome to Siml.ai University!</h1>
           <h2 className="font-light md:text-2xl md:pb-12">A place where you can learn everything about <strong className="font-bold">Siml.ai</strong> and find out how to utilize it for achieving incredible things!</h2>
 

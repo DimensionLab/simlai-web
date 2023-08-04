54ee0c144de1111cb544e7bf08abd1d295c04905
tohrxyz <tomas.hrib@proton.me>
2023-08-04 13:08:52 +0200

    feat: create and link manifest file [#159]

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
           <link rel="apple-touch-icon" href="https://siml.ai/assets/simlai/simlai-logo.svg" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />

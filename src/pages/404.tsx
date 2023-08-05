2b4f8779eec642b456e3851d5cd815d12e07eb94
tohrxyz <tomas.hrib@proton.me>
2023-08-05 20:42:15 +0200

    refactor: route to origin url (homepage)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,12 +8,14 @@ const PageNotFound = () => {
   const [seconds, setSeconds] = useState(15);
 
   useEffect(() => {
-    const currTime = seconds;
     setTimeout(() => {
-      setSeconds(currTime - 1);
+      setSeconds(seconds - 1);
     }, 1000);
 
-    seconds === 0 && router.push("../")
+    if (seconds === 0) {
+      const origin = window.location.origin;
+      router.push(origin);
+    }
   }, [seconds])
   
 
@@ -25,7 +27,7 @@ const PageNotFound = () => {
           <img src="/assets/simlai/simlai-title.svg" alt="" />
         </div>
         <div className="flex flex-col items-center gap-y-2">
-          <h1 className="text-3xl">Unfortunately, there's no <strong className="text-[#8B7CFF]">{pagePath}</strong> page.</h1>
+          <h1 className="text-3xl">Unfortunately, there&apos;s no <strong className="text-[#8B7CFF]">{pagePath}</strong> page.</h1>
           <p className="text-xl">You are going to be redirected to homepage in <strong className="text-[#8B7CFF]">{seconds}</strong> seconds.</p>
         </div>
       </main>

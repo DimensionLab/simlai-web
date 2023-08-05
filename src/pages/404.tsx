2e243edb5938109d3119c2180c009a0184f42e20
tohrxyz <tomas.hrib@proton.me>
2023-08-05 20:52:09 +0200

    refactor: customize responsivity

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -21,14 +21,14 @@ const PageNotFound = () => {
 
   return (
     <section className="grid w-full h-screen bg-[#0D101B]">
-      <main className="w-full flex flex-col items-center justify-center text-white gap-y-4">
+      <main className="w-full flex flex-col items-center justify-center text-white gap-y-4 px-4">
         <div className="flex flex-row items-end gap-x-2">
-          <img src="/assets/simlai/simlai-logo.svg" alt="" className="w-16"/>
-          <img src="/assets/simlai/simlai-title.svg" alt="" />
+          <img src="/assets/simlai/simlai-logo.svg" alt="" className="w-12 lg:w-16"/>
+          <img src="/assets/simlai/simlai-title.svg" alt="" className="w-32 lg:w-44"/>
         </div>
-        <div className="flex flex-col items-center gap-y-2">
-          <h1 className="text-3xl">Unfortunately, there&apos;s no <strong className="text-[#8B7CFF]">{pagePath}</strong> page.</h1>
-          <p className="text-xl">You are going to be redirected to homepage in <strong className="text-[#8B7CFF]">{seconds}</strong> seconds.</p>
+        <div className="flex flex-col items-center gap-y-2 text-center">
+          <h1 className="text-lg lg:text-3xl">Unfortunately, there&apos;s no <strong className="text-[#8B7CFF]">{pagePath}</strong> page.</h1>
+          <p className="text-sm lg:text-xl">You are going to be redirected to homepage in <strong className="text-[#8B7CFF]">{seconds}</strong> seconds.</p>
         </div>
       </main>
     </section>

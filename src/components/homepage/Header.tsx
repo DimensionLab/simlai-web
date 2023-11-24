f2fe2f7fd584c3d1ca7c998b0984ad0e1be8da29
tohrxyz <tomas.hrib@proton.me>
2023-11-24 10:40:42 +0100

    fix: open docs in new window

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -180,7 +180,7 @@ const Header = (props: HeaderProps) => {
           <Link href={"../#features"}>FEATURES</Link>
           <Link href={"../#model-engineer"}>MODEL ENGINEER</Link>
           <Link href={"../#simulation-studio"}>SIMULATION STUDIO</Link>
-          <a href={"https://docs.siml.ai"}>DOCS</a>
+          <a href={"https://docs.siml.ai"} target="_blank">DOCS</a>
           <Link href={"/pricing"}>
             <a className={`${props.whichSubpage === "pricing" ? `text-white` : ``}`}>PRICING</a>
           </Link>

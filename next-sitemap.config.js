b386724decf0eaf94bbe8be4a8de8c1237d7b729
tohrxyz <tomas.hrib@proton.me>
2024-03-10 17:24:31 +0100

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://siml.ai',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+  sitemapSize: 7000,
+  additionalPaths: async () => [
+    {
+      loc: 'https://docs.siml.ai',
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    }, 
+    {
+      loc: "https://meetings-eu1.hubspot.com/michal-bujnak",
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    }, 
+    {
+      loc: "https://www.dimensionlab.org/terms-and-conditions.html",
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: "https://www.dimensionlab.org/privacy-policy.html",
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    }
+  ]
+}

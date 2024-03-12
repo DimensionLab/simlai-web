59f5b49ba4405848e776e134299cba153d61f690
tohrxyz <tomas.hrib@proton.me>
2024-03-12 23:09:43 +0100

    feat: enable dynamic generation of metadata for university/blog articles, enabling url previews

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,7 +1,43 @@
 import { storyblokVersion } from "@/lib/environment";
 import { BlogStory } from "@/types/blog";
 import { ISbStoryParams, StoryblokStory, getStoryblokApi } from "@storyblok/react/rsc";
+import type { Metadata, ResolvingMetadata } from "next";
 import { redirect } from "next/navigation";
+
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+  const { data } = await fetchData(slug);
+  const { story }: { story: BlogStory } = data;
+
+  return {
+    title: story.name,
+    description: `Read the article ${story.name} on the Siml.ai Blog`,
+    openGraph: {
+      title: story.name,
+      description: `Read the article ${story.name} on the Siml.ai Blog`,
+      type: "article",
+      url: `https://siml.ai/blog/${story.slug}`,
+      images: [
+        {
+          url: story.content.image.filename,
+          alt: `Siml.ai Blog Article Image for ${story.name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@siml_ai",
+    },
+  }
+}
 
 export default async function BlogPostPage({ params }: { params: { slug: string[] } }) {
   const slug = params.slug.pop();
--- app/university/[...slug]/page.tsx
+++ app/university/[...slug]/page.tsx
@@ -3,7 +3,44 @@ import { storyblokVersion } from "@/lib/environment";
 import { UniversityPostCard } from "@/types/blog";
 import { ISbStoryParams, getStoryblokApi } from "@storyblok/react";
 import StoryblokStory from "@storyblok/react/story";
+import type { Metadata } from "next";
+import { ResolvingMetadata } from "next";
 import { redirect } from "next/navigation";
+
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+  const { data } = await fetchData(slug);
+  const { story }: { story: UniversityPostCard } = data;
+
+  return {
+    title: story.name,
+    description: `Read the article ${story.name} on the Siml.ai University`,
+    openGraph: {
+      title: story.name,
+      description: `Read the article ${story.name} on the Siml.ai University`,
+      type: "article",
+      url: `https://siml.ai/university/${story.slug}`,
+      images: [
+        {
+          url: story.content.thumbnail.filename,
+          alt: `Siml.ai University Article Image for ${story.name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@siml_ai",
+    },
+  }
+}
 
 export default async function UniversityPostPage({ params }: { params: { slug: string[] } }) {
   const slug = params.slug.pop();

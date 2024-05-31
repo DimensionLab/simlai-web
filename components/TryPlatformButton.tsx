e67148d7f36fda9ab4505dd08bd3850586d66ded
Michal Takáč <takacmichal92@gmail.com>
2024-05-31 12:23:22 +0300

    feat: styling changes

--- components/TryPlatformButton.tsx
+++ components/TryPlatformButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function TryPlatformButton() {
   return (
-    <Link href={"https://platform.siml.ai"} className="flex w-full my-10" target="_blank">
+    <Link href={"https://platform.siml.ai"} className="w-full my-2" target="_blank">
       <button className="bg-btnPurple px-10 py-3 font-bold text-xl rounded w-full">
         Try the platform now!
       </button>

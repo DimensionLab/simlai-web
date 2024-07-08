cc69da2532cfad99b41611a285e7cee2893dda54
Michal Takáč <takacmichal92@gmail.com>
2024-07-08 18:28:00 +0200

    feat: set perm redirects to DimensionLab website

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -18,13 +18,13 @@ module.exports = {
       lastmod: new Date().toISOString()
     }, 
     {
-      loc: "https://www.dimensionlab.org/terms-and-conditions.html",
+      loc: "https://www.dimensionlab.org/terms-and-conditions",
       changefreq: 'daily',
       priority: 0.7,
       lastmod: new Date().toISOString()
     },
     {
-      loc: "https://www.dimensionlab.org/privacy-policy.html",
+      loc: "https://www.dimensionlab.org/privacy-policy",
       changefreq: 'daily',
       priority: 0.7,
       lastmod: new Date().toISOString()
